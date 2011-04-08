0e18a74b5c3ea88716a31de85973b7b9101cf76c
Dustin Diaz <polvero@gmail.com>
2011-04-07 22:51:10 -0700

    damn this is legit

--- bonzo.js
+++ bonzo.js
@@ -6,12 +6,26 @@
   */
 !function (context) {
 
-  function noop() {
-
-  }
+  var doc = document,
+      html = (doc.compatMode == 'CSS1Compat') ?
+        doc.documentElement :
+        doc.body,
+      specialAttributes = /^checked|value$/,
+      stateAttributes = /^checked|selected$/,
+      ie = /msie/.test(navigator.userAgent);
 
   function classReg(c) {
     return new RegExp("(^|\\s+)" + c + "(\\s+|$)");
+  }
+
+  function each(ar, fn) {
+    for (i = 0, len = ar.length; i < len; i++) {
+      fn(ar[i]);
+    }
+  }
+
+  function trim(s) {
+    return s.replace(/(^\s*|\s*$)/g, '');
   }
 
   function camelize(s) {
@@ -21,23 +35,37 @@
   }
 
   function is(node) {
-    return node && node.nodeName && node.nodeType == 3;
+    return node && node.nodeName && node.nodeType == 1;
+  }
+
+  function some(ar, fn, scope) {
+    for (var i = 0, j = ar.length; i < j; ++i) {
+      if (fn.call(scope, ar[i], i, ar)) {
+        return true;
+      }
+    }
+    return false;
   }
 
   function _bonzo(elements) {
     this.elements = elements;
   }
 
-  function bonzo(els) {
-    return new _bonzo(els);
-  }
-
   _bonzo.prototype = {
+
     each: function (fn) {
       for (var i = 0; i  < this.elements.length; i++) {
-        fn(this.elements[i]);
+        fn.call(this, this.elements[i]);
       }
       return this;
+    },
+
+    map: function (fn) {
+      var m = [];
+      for (var i = 0; i  < this.elements.length; i++) {
+        m.push(fn.call(this, this.elements[i]));
+      }
+      return m;
     },
 
     html: function (html) {
@@ -45,23 +73,27 @@
         this.each(function (el) {
           el.innerHTML = html;
         }) :
-        el[0].innerHTML;
+        this.elements[0].innerHTML;
     },
 
     addClass: function (c) {
       return this.each(function (el) {
-        this.hasClass(el, c) || (el.className = $.trim(el.className + ' ' + c));
+        this.hasClass(el, c) || (el.className = trim(el.className + ' ' + c));
       });
     },
 
     removeClass: function (c) {
       return this.each(function (el) {
-        this.hasClass(el, c) && (el.className = $.trim(el.className.replace(classReg(c), ' ')));
+        this.hasClass(el, c) && (el.className = trim(el.className.replace(classReg(c), ' ')));
       });
     },
 
     hasClass: function (el, c) {
-      return classReg(c).test(el.className);
+      return typeof c == 'undefined' ?
+        some(this.elements, function (i) {
+          return classReg(el).test(i.className);
+        }) :
+        classReg(c).test(el.className);
     },
 
     show: function (elements) {
@@ -76,66 +108,171 @@
       });
     },
 
-    appendTo: function (node) {
-      return this.each(function (el) {
-        node.appendChild(el);
-      });
+    create: function (node) {
+      return typeof node == 'string' ?
+        function () {
+          var el = doc.createElement('div'), els = [];
+          el.innerHTML = node;
+          var nodes = el.childNodes;
+          el = el.firstChild;
+          els.push(el);
+          while (el = el.nextSibling) {
+            (el.nodeType == 1) && els.push(el);
+          }
+          return els;
+
+        }() : is(node) ? [node.cloneNode(true)] : [];
     },
 
     append: function (node) {
       return this.each(function (el) {
-        el.appendChild(node);
+        each(this.create(node), function (i) {
+          el.appendChild(i);
+        });
+      });
+    },
+
+    prepend: function (node) {
+      return this.each(function (el) {
+        var first = el.firstChild;
+        each(this.create(node), function (i) {
+          el.insertBefore(i, first);
+        });
+      });
+    },
+
+    before: function (node) {
+      return this.each(function (el) {
+        each(this.create(node), function (i) {
+          el.parentNode.insertBefore(i, el);
+        });
       });
     },
 
     after: function (node) {
       return this.each(function (el) {
-        node.parentNode.insertBefore(el, node.nextSibling);
+        each(this.create(node), function (i) {
+          el.parentNode.insertBefore(i, el.nextSibling);
+        });
       });
     },
 
     css: function (o, v) {
-      var fn = $.isString(o) ?
+      var fn = typeof o == 'string' ?
         function (el) {
-          el.style[o] = v;
+          el.style[camelize(o)] = v;
         } :
         function (el) {
           for (var k in o) {
-            o.hasOwnProperty(k) && (el.style[k] = o[k]);
+            o.hasOwnProperty(k) && (el.style[camelize(k)] = o[k]);
           }
         };
       return this.each(fn);
     },
 
     offset: function () {
-      return this.map(function (el) {
-        var width = el.offsetWidth;
-        var height = el.offsetHeight;
-        var top = el.offsetTop;
-        var left = el.offsetLeft;
-        while (el = el.offsetParent) {
-          top = top + el.offsetTop;
-          left = left + el.offsetLeft;
-        }
-        return {
-          top: top,
-          left: left,
-          height: height,
-          width: width
-        };
+      var el = this.elements[0];
+      var width = el.offsetWidth;
+      var height = el.offsetHeight;
+      var top = el.offsetTop;
+      var left = el.offsetLeft;
+      while (el = el.offsetParent) {
+        top = top + el.offsetTop;
+        left = left + el.offsetLeft;
+      }
+
+      return {
+        top: top,
+        left: left,
+        height: height,
+        width: width
+      };
+    },
+
+    attr: function (k, v) {
+      var el = this.elements[0];
+      return typeof v == 'undefined' ?
+        specialAttributes.test(k) ?
+          stateAttributes.test(k) && typeof el[k] == 'string' ?
+            true : el[k] : el.getAttribute(k) :
+        this.each(function (el) {
+          el.setAttribute(k, v);
+        });
+    },
+
+    remove: function () {
+      this.each(function (el) {
+        el.parentNode.removeChild(el);
       });
     },
 
-    attr: noop,
-    contains: noop,
-    clone: noop,
-    prepend: noop,
-    remove: noop,
-    replace: noop,
-    swap: noop,
-    docHeight: noop,
-    winHeight: noop
+    empty: function () {
+      return this.each(function (el) {
+        while (el.firstChild) {
+          el.removeChild(el.firstChild);
+        }
+      });
+    },
+
+    detach: function () {
+      return this.map(function (el) {
+        return el.parentNode.removeChild(el);
+      });
+    }
 
   };
+
+  function bonzo(els) {
+    return new _bonzo(els);
+  }
+
+  bonzo.augment = function (o, target) {
+    for (var k in o) {
+      o.hasOwnProperty(k) && ((target || _bonzo.prototype)[k] = o[k]);
+    }
+  };
+
+  bonzo.doc = function () {
+    var w = html.scrollWidth,
+        h = html.scrollHeight,
+        vp = this.viewport();
+    return {
+      width: Math.max(w, vp.width),
+      height: Math.max(h, vp.height)
+    };
+  };
+
+  bonzo.viewport = function () {
+    var h = self.innerHeight,
+        w = self.innerWidth;
+    ie && (h = html.clientHeight) && (w = html.clientWidth);
+    return {
+      width: w,
+      height: h
+    };
+  };
+
+  bonzo.contains = 'compareDocumentPosition' in html ?
+    function (container, element) {
+      return (container.compareDocumentPosition(element) & 16) == 16;
+    } : 'contains' in html ?
+    function (container, element) {
+      return container !== element && container.contains(element);
+    } :
+    function (container, element) {
+      while (element = element.parentNode) {
+        if (element === container) {
+          return true;
+        }
+      }
+      return false;
+    };
+
+  var old = context.bonzo;
+  bonzo.noConflict = function () {
+    context.bonzo = old;
+    return this;
+  };
+  context.bonzo = bonzo;
 
 }(this);
