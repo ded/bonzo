1d48c1a6fe1630d1e10a5c54d556deed5e27b6fb
Dustin Diaz <polvero@gmail.com>
2011-04-19 16:08:52 -0700

    add parents() method to ender integration

--- src/ender.js
+++ src/ender.js
@@ -7,6 +7,29 @@
       return $(b.create(node));
     }
   });
+  $.ender({
+    parents: function (selector) {
+      var collection = $(selector), i, l, j, k, r = [];
+      collect:
+      for (i = 0, l = collection.length; i < l; i++) {
+        for (j = 0, k = this.length; j < k; j++) {
+          if (b.isAncestor(collection[i], this[j])) {
+            r.push(collection[i]);
+            continue collect;
+          }
+        }
+      }
+      this.elements = [];
+      for (i = 0, l = this.length; i < l; i++) {
+        delete this[i];
+      }
+      for (i = 0, l = r.length; i < l; i++) {
+        this[i] = r[i];
+      }
+      this.length = r.length;
+      return this;
+    }
+  }, true);
 
 }();
 
