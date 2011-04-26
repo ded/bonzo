a5231d2b871b5310954031f645384d68995dc0db
Dustin Diaz <polvero@gmail.com>
2011-04-26 14:09:25 -0700

    siblings()

--- src/ender.js
+++ src/ender.js
@@ -58,6 +58,21 @@
 
     previous: function () {
       return $(b(this).previous());
+    },
+
+    siblings: function () {
+      var i, l, p, r = [];
+      for (i = 0, l = this.length; i < l; i++) {
+        p = this[i];
+        while (p = p.previousSibling) {
+          p.nodeType == 1 && r.push(p);
+        }
+        p = this[i];
+        while (p = p.nextSibling) {
+          p.nodeType == 1 && r.push(p);
+        }
+      }
+      return $(uniq(r));
     }
   }, true);
 
