11f4e7eb27ebe65eec6dc6df57b5fe816904b979
Dustin Diaz <polvero@gmail.com>
2011-04-26 11:09:43 -0700

    make html set html when parameter is not undefined

--- src/bonzo.js
+++ src/bonzo.js
@@ -80,7 +80,7 @@
     },
 
     html: function (html) {
-      return typeof html == 'string' ?
+      return typeof html !== 'undefined' ?
         this.each(function (el) {
           el.innerHTML = html;
         }) :
