697bbd287490a3aa33b6fc91e17ffe99194941aa
Connor Montgomery <connor@connormontgomery.com>
2011-12-07 22:59:50 -0800

    Only focus the first item in the selection

--- src/bonzo.js
+++ src/bonzo.js
@@ -389,6 +389,16 @@
         })
       }
 
+    , focus: function () {
+        return this[0].focus()
+      }
+
+    , blur: function () {
+        return this.each(function (el) {
+          el.blur()
+        })
+      }
+
     , css: function (o, v, p) {
         // is this a request for just getting a style?
         if (v === undefined && typeof o == 'string') {
