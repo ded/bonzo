ec6b4ccda3520fb3407ed4d7b1c96d91047fd551
Connor Montgomery <connor@connormontgomery.com>
2011-12-08 20:41:00 -0800

    realized what i did was working, but wasn't necessarily correct.

--- src/bonzo.js
+++ src/bonzo.js
@@ -390,7 +390,7 @@
       }
 
     , focus: function () {
-        return this.length != 0 ? this[0].focus() : null
+        return this.length > 0 ? this[0].focus() : null
       }
 
     , blur: function () {
