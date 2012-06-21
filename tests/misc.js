776a849acc8dd5a244942cb02e41005aea014de5
Rod Vagg <rod@vagg.org>
2012-06-21 16:20:59 +1000

    split out traversal tests

--- tests/misc.js
+++ tests/misc.js
@@ -17,16 +17,4 @@ sink('Miscellaneous', function(test, ok, before, after) {
     ok($('#first-last div').first()[0].id == 'first', 'found first() element')
     ok($('#first-last div').last()[0].id == 'last', 'found last() element')
   })
-
-  test('next', 1, function () {
-    ok($('#sibling-tests li.nextr').next().length == 2, 'els.next().length == 2')
-  })
-
-  test('previous', 1, function () {
-    ok($('#sibling-tests li.nextr').previous().length == 3, 'els.previous().length == 3')
-  })
-
-  test('parent', 1, function () {
-    ok($('#parent-test').parent()[0].id == 'parent-test-wrapper', 'parent() is parent-test-wrapper')
-  })
 })
