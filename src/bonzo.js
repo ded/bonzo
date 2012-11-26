d757fe64026b93bfde2a55c5f4f6ed8bf456ca99
Dustin Diaz <polvero@gmail.com>
2012-11-26 08:55:25 -0800

    actual support for requirejs and amd

--- src/bonzo.js
+++ src/bonzo.js
@@ -1,6 +1,6 @@
 (function (name, context, definition) {
   if (typeof module != 'undefined' && module.exports) module.exports = definition()
-  else if (typeof context['define'] == 'function' && context['define']['amd']) define(name, definition)
+  else if (typeof context['define'] == 'function' && context['define']['amd']) define(definition)
   else context[name] = definition()
 })('bonzo', this, function() {
   var win = window
