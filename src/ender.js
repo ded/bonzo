1fb8dcac14d519ce00ba24c6a652c64cbda89270
Michael Hu <humingchun@gmail.com>
2013-07-17 15:19:25 +0800

    Make sure the elements returned by siblings() are in the same order as in the DOM

--- src/ender.js
+++ src/ender.js
@@ -103,6 +103,7 @@
       for (i = 0, l = this.length; i < l; i++) {
         p = this[i]
         while (p = p.previousSibling) p.nodeType == 1 && r.push(p)
+        r.reverse()
         p = this[i]
         while (p = p.nextSibling) p.nodeType == 1 && r.push(p)
       }
